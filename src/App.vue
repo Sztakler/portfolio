3694696c9ec4274d598b421a5b7e6401b5eb1cb3
metarth <krystian.jasionek@gmail.com>
2024-06-14 21:07:41 +0200

    Added workflow for Vue

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
 
 <template>
   <header>
-    <nav>
+    <nav class="red-hat-display-semibold">
       <ul>
         <li>
-          <a href="/about-me">About me_</a>
+          <a href="/about-me">About_me_</a>
         </li>
         <li>
           <a href="/work">Work_</a>
@@ -20,10 +20,84 @@
 
   <main>
     <header>
-      <h1>Krystian Jasionek</h1>
-      <h2>Designer and Developer</h2>
+      <h1>
+        Krystian Jasionek
+        <div class="subtitle">{Designer and Developer}</div>
+      </h1>
     </header>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+@import "./style.css";
+
+header>nav {
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+  color: black;
+  text-transform: uppercase;
+  font-size: 2.5rem;
+}
+
+header>nav>h2 {
+  all: unset;
+  padding: 0.5rem;
+}
+
+header>nav>ul {
+  all: unset;
+  display: flex;
+  justify-self: center;
+  gap: 1rem;
+}
+
+ul>li {
+  all: unset;
+  padding: 0.5rem;
+}
+
+ul>li>a {
+  all: unset;
+}
+
+ul>li:hover {
+  background-color: white;
+  color: black;
+}
+
+nav>h2:hover {
+  background-color: white;
+  color: black;
+}
+
+main>header {
+  writing-mode: vertical-rl;
+  text-orientation: mixed;
+  text-transform: uppercase;
+  padding: 2rem;
+}
+
+main>header>h1 {
+  margin: 0;
+  padding: 0;
+  font-size: 7.5vw;
+  font-family: "Noto Sans Mono", monospace;
+  font-weight: 900;
+  font-style: normal;
+  position: relative;
+  color: darkred;
+  mix-blend-mode: difference;
+
+}
+
+.subtitle {
+  all: unset;
+  margin: 0;
+  font-size: 1.5vw;
+  position: absolute;
+  left: 20%;
+  top: 35%;
+  color: white;
+}
+</style>
