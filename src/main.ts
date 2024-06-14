07b4c1e5add182b08e4d331dec5de28e7b4e193a
metarth <krystian.jasionek@gmail.com>
2024-06-14 22:24:31 +0200

    Changed vue config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { AnimatedBackground} from "./canvas";
+// import { createApp } from 'vue'
+// import './style.css'
+// import App from './App.vue'
 
-const animatedBackground = new AnimatedBackground("#canvas", 'black');
+// createApp(App).mount('#app')
 
-createApp(App).mount('#app')
-
-animatedBackground.render();
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    publicPath: '/portfolio/'
-}
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/portfolio/' // Replace with your GitHub repo name
+    : '/'
+};
